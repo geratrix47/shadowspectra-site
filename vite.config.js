0e8c2d295f1dcc654a927b40b0a1eac0351e8823
geratrix47 <hemerson.barbosa@catolica.edu.br>
2025-06-24 22:04:44 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/shadowspectra-site/', // NOME EXATO DO TEU REPOSITÓRIO
   plugins: [react()],
-  base: '/shadowspectra-site/',
 })
